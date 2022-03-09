1979dff3dcc94490ddfae628c1262f65276aa2c0
Fadimata Tillman <fadi.tillman@gmail.com>
2022-03-09 11:48:33 -0800

    add  rectangle and circle shapes to wold

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // console.log('Bonjour')
 const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
+const width = 800;
+const height = 600;
+
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -34,4 +38,20 @@ const walls = [
 ]
     World.add(world, walls);
 
-    World.add(world, Bodies.rectangle(200, 200, 50, 50));
+    //random
+for (let i = 0; i < 20; i++ ){
+    if(Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    
+        );
+    
+    } else {
+        World.add(
+        world,
+        Bodies.circle(Math.random() * width, Math.random() * height, 35)
+        )
+    }
+    
+    }
+    
+   
