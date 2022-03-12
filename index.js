b2c877c6825e3c39947f479f8f920590f52db271
Fadimata Tillman <fadi.tillman@gmail.com>
2022-03-12 14:56:45 -0800

    refomate

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // console.log('Bonjour')
-const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+const {Engine, Render, Runner, World, Bodies} = Matter;
+//MouseConstraint, Mouse
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 
@@ -13,15 +14,15 @@ const render = Render.create({
     options:{
         wireframes: false,
         width:800,
-        height: 600
+        height: 600,
     }
 })
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine,{
-    mouse: Mouse.create(render.canvas)
-}))
+// World.add(world, MouseConstraint.create(engine,{
+//     mouse: Mouse.create(render.canvas)
+// }))
 
 // const shape = Bodies.rectangle(200,200,50,50,{
 //     //remove gravity
@@ -32,27 +33,31 @@ World.add(world, MouseConstraint.create(engine,{
 
 //walls
 const walls = [
-    Bodies.rectangle(400, 0,800,40,{isStatic: true}),
-    Bodies.rectangle(400, 600,800,40,{isStatic: true}),
-    Bodies.rectangle(0,300,40,600, {isStatic: true}),
-    Bodies.rectangle(800,300,40,600, {isStatic: true})
+    Bodies.rectangle(width/2, 0, width,40,{isStatic: true}),
+    Bodies.rectangle(width/2, height,width,40,{isStatic: true}),
+    Bodies.rectangle(0,height/2,40,height, {isStatic: true}),
+    Bodies.rectangle(width,height/2,40,height, {isStatic: true})
 ]
     World.add(world, walls);
 
     //random
-for (let i = 0; i < 50; i++ ){
-    if(Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+// for (let i = 0; i < 50; i++ ){
+//     if(Math.random() > 0.5) {
+//         World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
     
-        );
+//         );
     
-    } else {
-        World.add(
-        world,
-        Bodies.circle(Math.random() * width, Math.random() * height, 35)
-        )
-    }
+//     } else {
+//         World.add(
+//         world,
+//         Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+//             render:{
+//                 fillStyle: 'red'
+//             }
+//         })
+//         )
+//     }
     
-    }
+//     }
     
    
